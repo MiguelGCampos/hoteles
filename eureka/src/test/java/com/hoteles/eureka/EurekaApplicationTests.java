d4aa1dcf4ecc11b5052d189c5eb7c2854da23a6e
Miguel G. Campos <miguelangelgallegoscampos@gmail.com>
2026-07-24 20:02:10 -0600

    Modulos creados VACIOS, PENDIENTE LLENAR

---
+++ eureka/src/test/java/com/hoteles/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoteles.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
